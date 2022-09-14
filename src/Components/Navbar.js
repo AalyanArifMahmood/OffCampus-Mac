e61bc3f0b9b83798f211aab3f16b054fcb612ec6
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-14 16:05:10 -0500

    Just some initial UI stuff

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 function render() {
   return (
-    <div>
-
+    <div style={{width: '100%' ,backgroundColor: 'blue', top: '0px', position: 'absolute'}}>
+      <h3 style={{color: 'white', textAlign: 'center'}}>Find apartments to Sublet!</h3>
     </div>
   );
 }
