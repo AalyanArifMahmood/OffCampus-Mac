7878561cc4778addb869d431567216361855d7bd
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-14 15:56:13 -0500

    Added Bootstrap and FluentUI dependencies

---
+++ src/Components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function render() {
+  return (
+    <div>
+
+    </div>
+  );
+}
+
+export default render;
--- src/testFiles/App.test.js
+++ src/testFiles/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../Components/App';
+import App from '../Components/Navbar';
 
 test('renders learn react link', () => {
   render(<App />);
