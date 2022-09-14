27d65fa49d0de764fb3fdec3594c38274b5d09ab
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-14 15:56:13 -0500

    Added Bootstrap and FluentUI dependencies

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './CSS/index.css';
-import App from './Components/App';
+import Navbar from './Components/Navbar';
 import reportWebVitals from './testFiles/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Navbar />
   </React.StrictMode>
 );
 
