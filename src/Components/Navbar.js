ccbda16140a8fdb2f4d259df991e59de5ee31410
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-14 16:18:48 -0500

    Using React Class components!

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
-function render() {
-  return (
-    <div style={{width: '100%' ,backgroundColor: 'blue', top: '0px', position: 'absolute'}}>
-      <h3 style={{color: 'white', textAlign: 'center'}}>Find apartments to Sublet!</h3>
-    </div>
-  );
+class NavBar extends React.Component
+{
+    render()
+    {
+        return (
+            <div style={{width: '100%' ,backgroundColor: 'blue', top: '0px', position: 'absolute'}}>
+                <h3 style={{color: 'white', textAlign: 'center'}}>Find apartments to Sublet!</h3>
+            </div>
+        );
+    }
 }
 
-export default render;
+export default NavBar
+
+
