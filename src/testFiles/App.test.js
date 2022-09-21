da34cfd1b0c52819be8a67982c105bf225d8f6f0
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-21 14:00:57 -0500

    Navbar lookin good

--- src/testFiles/App.test.js
+++ src/testFiles/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from '../Components/Navbar';
+import App from '../Components/Navbar/Navbar';
 
 test('renders learn react link', () => {
   render(<App />);
