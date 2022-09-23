c3681419f54c0b522feef806636b9e62395431a9
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-23 12:02:06 -0500

    Lookin good!

---
+++ src/Components/MainPage/Main_Page.js
@@ -0,0 +1,11 @@
+
+
+function Info() {
+    return (
+        <>
+            <h1 style={{fontStyle: 'montserrat', textAlign: 'center', marginTop: '5%'}}>Find Listings Around Macalester College</h1>
+        </>
+    );
+}
+
+export default Info;
