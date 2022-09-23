58f0e34f3b6101be32d4131cf896dd9fd14aad61
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-23 11:21:11 -0500

    Navbar changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -14,13 +14,13 @@ export default function Navbar() {
         <nav className="desktop-navigation">
             <h2>OffCampus@Mac</h2>
             <Scrollspy
-                items={['home', 'listings', 'about', 'contact']}
+                items={['home', 'listings', 'contact', 'profile']}
                 currentClassName="active"
             >
-                <li><a href="#home">Home</a></li>
-                <li><a href="#listings">Listings</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#contact">Contact</a></li>
+                <li><a href="home">Home</a></li>
+                <li><a href="listings">Listings</a></li>
+                <li><a href="contact">Contact</a></li>
+                <li><a href="profile">Profile</a></li>
             </Scrollspy>
         </nav>
     );
