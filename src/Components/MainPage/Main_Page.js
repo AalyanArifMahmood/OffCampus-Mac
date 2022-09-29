fe34dfaca474e35b118e759f0dd74cfd09a26d3e
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-09-28 19:01:31 -0500

    Logo

--- src/Components/MainPage/Main_Page.js
+++ src/Components/MainPage/Main_Page.js
@@ -2,15 +2,17 @@ import image from './Background1.jpeg'
 import '../../CSS/index.css'
 import React from 'react';
 import '../../CSS/Fading.css'
+import image2 from "../../Resources/MacLogo.png";
 
 function Info() {
     return (
         <>
-            <div >
+            <div>
                 <section id={"home"}>
-                <img src={image} alt="Nothing" style={{width: '1440px', height: '755px', filter: 'brightness(50%)', boxShadow: '1px 12px 9px #6f6f6f'}}/>
-                    <div style={{textAlign: 'center', position: 'absolute', paddingLeft: '12%',bottom: '35%',paddingRight: '10%', color: 'white'}} class={"fade-in-text"}>
-                        <h1 style={{fontSize: '9vh', fontFamily: 'Newslab, georgia, Bakersville'}}>Find Listings Around Macalester College</h1>
+                <img src={image} alt="Nothing" style={{width: '100%', height: '100%', filter: 'brightness(40%)', boxShadow: '1px 12px 9px #6f6f6f'}}/>
+                    <div style={{display: 'flex', textAlign: 'center', position: 'absolute', paddingLeft: '12%',bottom: '21%', paddingRight: '10%', color: 'white'}} class={"fade-in-text"}>
+                        <img src={image2} alt="Nothing" style={{height: '30%', width: '30%'}}/>
+                        <h1 style={{marginTop: '8%', fontSize: '9vh', fontFamily: 'Newslab, georgia, Bakersville'}}>Find Listings Around Macalester College</h1>
                     </div>
                 </section>
             </div>
--- src/Components/Profile/profile.js
+++ src/Components/Profile/profile.js
@@ -1,5 +1,4 @@
 import '../../CSS/index.css'
-import image from "../MainPage/Background1.jpeg";
 
 
 
@@ -14,7 +13,6 @@ function Profile() {
 
                 </div>
             </div>
-            <img src={image} alt="Nothing" style={{width: '1440px', height: '755px', filter: 'brightness(50%)', boxShadow: '1px 12px 9px #6f6f6f'}}/>
             <div className="separator" />
         </>
     );
