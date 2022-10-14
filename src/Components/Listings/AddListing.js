eb028a77391bd3449a50a70f4a5592f95dc5b075
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-10-14 10:58:14 -0500

    Bunch of updates

--- src/Components/Listings/AddListing.js
+++ src/Components/Listings/AddListing.js
@@ -6,8 +6,8 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import {TextField} from "@fluentui/react";
 import { Stack, IStackProps } from '@fluentui/react/lib/Stack';
-import { ScrollablePane, IScrollablePaneStyles } from '@fluentui/react/lib/ScrollablePane';
-import {getTheme, mergeStyleSets} from "@fluentui/react/lib/Styling";
+import { getStorage, ref as reff, uploadBytes } from "firebase/storage";
+import {DropzoneDialog} from 'material-ui-dropzone'
 
 const firebaseConfig = {
     apiKey: "AIzaSyCxyu7ou0WSzQYk63StiYtCVG-XtUPpqNs",
@@ -23,7 +23,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
-
+const storage = getStorage();
+const storageRef = reff(storage, 'some-child');
 
 const columnProps: Partial<IStackProps> = {
     tokens: { childrenGap: 15 },
@@ -37,32 +38,16 @@ export interface IScrollablePaneExampleItem {
     rent: number;
     email: string;
 }
-const theme = getTheme();
-const classNames = mergeStyleSets({
-    wrapper: {
-        height: '40vh',
-        position: 'relative',
-        maxHeight: 'inherit',
-    },
-    pane: {
-        maxWidth: 400,
-        border: '1px solid ' + theme.palette.neutralLight
-    },
-    textContent: {
-        padding: '15px 10px',
-    },
-});
 
-const scrollablePaneStyles: Partial<IScrollablePaneStyles> = { root: classNames.pane };
-
-const createContentArea = (item: IScrollablePaneExampleItem) => (
-    <div >
-        <div className={classNames.textContent} style={{backgroundColor: 'gray'}}>
-            <SingleList address={item.address} name={item.name} rent={item.rent} email={item.email}/>
-        </div>
-    </div>
-);
-
+// const scrollablePaneStyles: Partial<IScrollablePaneStyles> = { root: classNames.pane };
+//
+// const createContentArea = (item: IScrollablePaneExampleItem) => (
+//     <div >
+//         <div className={classNames.textContent}>
+//             <SingleList address={item.address} name={item.name} rent={item.rent} email={item.email}/>
+//         </div>
+//     </div>
+// );
 
 // **************************************   This is where the class starts   *******************************************
 class AddListings extends React.Component
@@ -72,7 +57,10 @@ class AddListings extends React.Component
         this.state=
         {
             listings: [],
-            able: false
+            able: false,
+            listAddresses: [],
+            files: [],
+            open: false
         }
     }
 
@@ -86,39 +74,76 @@ class AddListings extends React.Component
 
         const listRef = ref(database, 'items');
         let tempListings = []
+        let tempAddresses = []
         onValue(listRef, (snapshot) => {
             snapshot.forEach(function(childSnapshot)
             {
                 tempListings.push(childSnapshot.val())
+                tempAddresses.push(childSnapshot.val().address)
             })
         });
         this.setState(prevState => ({
             listings: [...prevState.listings, tempListings]
         }))
+        this.setState(prevState => ({
+            listAddresses: [...prevState.listAddresses, tempAddresses]
+        }))
+    }
+
+    handleClose() {
+        this.setState({
+            open: false
+        });
+    }
+
+    handleSave(files) {
+        //Saving files to state for further use and closing Modal.
+        this.setState({
+            files: files,
+            open: false
+        });
+    }
+
+    handleOpen() {
+        this.setState({
+            open: true,
+        });
     }
 
     writeUserData = () => {
         if (!(document.getElementById("addressBox").value === "") && !(document.getElementById("nameBox").value === "") &&
             !(document.getElementById("rentBox").value === "") && !(document.getElementById("emailBox").value === ""))
         {
-            set(ref(database, 'items/' + document.getElementById("addressBox").value), {
-                name: document.getElementById("nameBox").value,
-                email: document.getElementById("emailBox").value,
-                address: document.getElementById("addressBox").value,
-                rent: document.getElementById("rentBox").value
-            });
-            alert("Success! Please refresh page to see changes!")
+            if(this.state.listAddresses[0].includes(document.getElementById("addressBox").value))
+            {
+                alert("This listing already exists");
+            }
+            else
+            {
+                uploadBytes(storageRef, this.state.files[0]).then((snapshot) => {
+                    console.log('Uploaded a blob or file!');
+                });
+                set(ref(database, 'items/' + document.getElementById("addressBox").value), {
+                    name: document.getElementById("nameBox").value,
+                    email: document.getElementById("emailBox").value + "@macalester.edu",
+                    address: document.getElementById("addressBox").value,
+                    rent: document.getElementById("rentBox").value,
+                    photo: uploadBytes(storageRef, this.state.files[0])
+                });
+                alert("Success! Please refresh page to see changes!")
+            }
         }
     }
 
     enable = () =>
     {
-        this.readUserData();
+        // this.readUserData();
         this.setState(prevState => ({able: !prevState.able}));
     }
 
     render()
     {
+        console.log(this.state.files)
         if(!this.state.able)
         {
             // *********************************************   BEFORE RE RENDER   **************************************
@@ -149,30 +174,47 @@ class AddListings extends React.Component
                      </div>
                      <Stack {...columnProps}>
                          <TextField label="Name " required id={"nameBox"} onGetErrorMessage={value => {
-                             if (value === '') {
+                             if (value === "") {
                                  return 'This field is required';
                              }
                          }}/>
                          <TextField label="Listing Address" required id={"addressBox"} onGetErrorMessage={value => {
-                             if (value === '') {
+                             if (value==="") {
                                  return 'This field is required';
                              }
                          }}/>
-                         <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"}
+                         <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"} suffix="@macalester.edu"
                                     onGetErrorMessage={value => {
-                                        if (value === '') {
+                                        if (value==="") {
                                             return 'This field is required';
                                         }
                                     }}/>
                          <TextField label="Rent" required id={"rentBox"} onGetErrorMessage={value => {
-                             if (value === '') {
+                             if (value==="") {
                                  return 'This field is required';
                              }
                          }}/>
                      </Stack>
                      <br/>
-                     <PrimaryButton text="Add Listing" onClick={this.writeUserData} style={{marginLeft: "45%"}} allowDisabledFocus/>
+                     <div style={{display: "flex"}}>
+                     <div style={{marginLeft: '40%'}}>
+                         <PrimaryButton onClick={this.handleOpen.bind(this)}>
+                             Add Image
+                         </PrimaryButton>
+                         <DropzoneDialog
+                             open={this.state.open}
+                             onSave={this.handleSave.bind(this)}
+                             acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
+                             showPreviews={true}
+                             maxFileSize={5000000}
+                             onClose={this.handleClose.bind(this)}
+                         />
+                     </div>
 
+
+                     <br/>
+                     <PrimaryButton text="Add Listing" onClick={this.writeUserData} style={{marginLeft: "5%", backgroundColor: 'green'}} allowDisabledFocus/>
+                     </div>
                      <div className="separator"/>
                  </>
              );
@@ -192,22 +234,20 @@ class AddListings extends React.Component
                                 }}>Available Listings</h1>
                             </section>
                             <PrimaryButton text="Hide Listings" onClick={this.enable}  allowDisabledFocus/>
-                            <div >
-                                {/*<ScrollablePane scrollContainerFocus={true} scrollContainerAriaLabel="Sticky component example" styles={scrollablePaneStyles}>*/}
-                                {/*    {this.state.listings.map(createContentArea)}*/}
-                                {/*</ScrollablePane>*/}
-                            </div>
+                            {/*<div >*/}
+                            {/*    /!*<ScrollablePane scrollContainerFocus={true} scrollContainerAriaLabel="Sticky component example"  styles={scrollablePaneStyles} scrollbarVisibility={ScrollbarVisibility}>*!/*/}
+                            {/*    /!*    {this.state.listings[0].map(createContentArea)}*!/*/}
+                            {/*    /!*</ScrollablePane>*!/*/}
+                            {/*</div>*/}
                         </div>
                     </div>
                     <ul>
                         {this.state.listings[0].map((data) => (
                             <li key={data.address}>
-                                <SingleList name={data.name} address={data.address} email={data.email} rent={data.rent}/>
+                                <SingleList name={data.name} address={data.address} email={data.email} rent={data.rent} image={data.photo}/>
                             </li>
                             ))}
                     </ul>
-                    {/*<SingleList name={this.state.listings[0][1].name} rent={this.state.listings[0][0].rent} email={this.state.listings[0][0].email} address={this.state.listings[0][0].address}/>*/}
-                    {/*<SingleList name={this.state.listings[0][1].name} rent={this.state.listings[0][1].rent} email={this.state.listings[0][1].email} address={this.state.listings[0][1].address}/>*/}
                     <div className="separator"/>
 
                     <div style={{position: 'relative'}}>
@@ -220,29 +260,45 @@ class AddListings extends React.Component
                     </div>
                     <Stack {...columnProps}>
                         <TextField label="Name " required id={"nameBox"} onGetErrorMessage={value => {
-                            if (value === '') {
+                            if (value==="") {
                                 return 'This field is required';
                             }
-                        }}/>
+                        }} />
                         <TextField label="Listing Address" required id={"addressBox"} onGetErrorMessage={value => {
-                            if (value === '') {
+                            if (value==="") {
                                 return 'This field is required';
                             }
                         }}/>
-                        <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"}
+                        <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"} suffix="@macalester.edu"
                                    onGetErrorMessage={value => {
-                                       if (value === '') {
+                                       if (value==="") {
                                            return 'This field is required';
                                        }
                                    }}/>
                         <TextField label="Rent" required id={"rentBox"} onGetErrorMessage={value => {
-                            if (value === '') {
+                            if (value==="") {
                                 return 'This field is required';
                             }
                         }}/>
                     </Stack>
                     <br/>
-                    <PrimaryButton text="Add Listing" onClick={this.writeUserData} style={{marginLeft: "45%"}} allowDisabledFocus/>
+                    <div style={{display: 'flex'}}>
+                    <div style={{marginLeft: '40%'}}>
+                        <PrimaryButton onClick={this.handleOpen.bind(this)}>
+                            Add Image
+                        </PrimaryButton>
+                        <DropzoneDialog
+                            open={this.state.open}
+                            onSave={this.handleSave.bind(this)}
+                            acceptedFiles={['image/jpeg', 'image/png', 'image/bmp']}
+                            showPreviews={true}
+                            maxFileSize={5000000}
+                            onClose={this.handleClose.bind(this)}
+                        />
+                    </div>
+                    <br/>
+                    <PrimaryButton text="Add Listing" onClick={this.writeUserData} style={{marginLeft: "5%", backgroundColor: 'green'}} allowDisabledFocus/>
+                    </div>
                     <div className="separator"/>
                 </>
             );
