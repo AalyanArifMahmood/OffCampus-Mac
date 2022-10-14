2bb484fc22e973cce300ea0293252eae22c037bb
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-10-14 10:58:14 -0500

    Bunch of updates

--- src/Components/Listings/SingleListingDisplay.js
+++ src/Components/Listings/SingleListingDisplay.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import Box from '@material-ui/core/Box';
+import image from "../MainPage/Background1.jpeg";
+import { Stack} from '@fluentui/react/lib/Stack';
 
 class SingleList extends React.Component
 {
@@ -6,13 +9,17 @@ class SingleList extends React.Component
     {
         return (
             <>
-            <div style={{textAlign: 'center'}}>
-                <p><b>Listing Address:</b> {this.props.address} </p>
-                <p><b>Lister's Name:</b> {this.props.name}</p>
-                <p><b>Lister's Contact:</b> {this.props.email}  </p>
-                <p><b>Listing Rent:</b> {this.props.rent}  </p>
-            </div>
-            <br/>
+                <Box bgcolor="white" p={1} border={2} marginRight={10} marginLeft={10}>
+                    <div style={{justifyContent: "space-between", display: "flex"}}>
+                        <Stack>
+                            <p><b>Listing Address:</b> {this.props.address} </p>
+                            <p><b>Lister's Name:</b> {this.props.name}</p>
+                            <p><b>Lister's Contact:</b> {this.props.email}  </p>
+                            <p><b>Listing Rent:</b> {this.props.rent}  </p>
+                        </Stack>
+                        <img src={image} alt="Nothing" style={{width: '38vh', height: '25vh'}}/>
+                    </div>
+                </Box>
             </>
         );
     }
