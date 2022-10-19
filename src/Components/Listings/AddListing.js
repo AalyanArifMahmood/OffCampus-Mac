132aeaa2b33c2526646ef69563f7bc53f4d2434f
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-10-19 10:57:17 -0500

    Major EUREKAS

--- src/Components/Listings/AddListing.js
+++ src/Components/Listings/AddListing.js
@@ -6,7 +6,7 @@ import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import {TextField} from "@fluentui/react";
 import { Stack, IStackProps } from '@fluentui/react/lib/Stack';
-import { getStorage, ref as reff, uploadBytes } from "firebase/storage";
+import { getStorage, ref as reff, uploadBytes, getDownloadURL } from "firebase/storage";
 import {DropzoneDialog} from 'material-ui-dropzone'
 
 const firebaseConfig = {
@@ -24,12 +24,26 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 const storage = getStorage();
-const storageRef = reff(storage, 'some-child');
 
 const columnProps: Partial<IStackProps> = {
     tokens: { childrenGap: 15 },
-    styles: { root: { width: 300, marginLeft: '40%' } },
+    styles: { root: { width: 300, marginLeft: '30%' } },
 };
+
+const columnProps2: Partial<IStackProps> = {
+    tokens: { childrenGap: 15 },
+    styles: { root: { width: 300, marginLeft: '5%' } },
+};
+
+function makeid(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
 
 
 export interface IScrollablePaneExampleItem {
@@ -110,7 +124,8 @@ class AddListings extends React.Component
         });
     }
 
-    writeUserData = () => {
+    writeUserData = () =>
+    {
         if (!(document.getElementById("addressBox").value === "") && !(document.getElementById("nameBox").value === "") &&
             !(document.getElementById("rentBox").value === "") && !(document.getElementById("emailBox").value === ""))
         {
@@ -120,18 +135,26 @@ class AddListings extends React.Component
             }
             else
             {
+                const string = makeid(10);
+                const storageRef = reff(storage, string);
                 uploadBytes(storageRef, this.state.files[0]).then((snapshot) => {
                     console.log('Uploaded a blob or file!');
+                    getDownloadURL(storageRef).then((url) =>
+                    {
+                        set(ref(database, 'items/' + document.getElementById("addressBox").value), {
+                            description: document.getElementById("descriptionBox").value,
+                            name: document.getElementById("nameBox").value,
+                            email: document.getElementById("emailBox").value + "@macalester.edu",
+                            address: document.getElementById("addressBox").value,
+                            rent: document.getElementById("rentBox").value,
+                            photo: url,
+                            details: document.getElementById("detailBox").value
+                        });
+
+                        alert("Success! Please refresh page to see changes!")
+                    })
                 });
-                set(ref(database, 'items/' + document.getElementById("descriptionBox").value), {
-                    description: document.getElementById("descriptionBox").value,
-                    name: document.getElementById("nameBox").value,
-                    email: document.getElementById("emailBox").value + "@macalester.edu",
-                    address: document.getElementById("addressBox").value,
-                    rent: document.getElementById("rentBox").value,
-                    photo: uploadBytes(storageRef, this.state.files[0])
-                });
-                alert("Success! Please refresh page to see changes!")
+
             }
         }
     }
@@ -173,34 +196,43 @@ class AddListings extends React.Component
                              </section>
                          </div>
                      </div>
-                     <Stack {...columnProps}>
-                         <TextField label="Short Description/Name of Listing" multiline autoAdjustHeight required id={"descriptionBox"} onGetErrorMessage={value => {
-                             if (value === "") {
-                                 return 'This field is required';
-                             }
-                         }}/>
-                         <TextField label="Name " required id={"nameBox"} onGetErrorMessage={value => {
-                             if (value === "") {
-                                 return 'This field is required';
-                             }
-                         }}/>
-                         <TextField label="Listing Address" required id={"addressBox"} onGetErrorMessage={value => {
-                             if (value==="") {
-                                 return 'This field is required';
-                             }
-                         }}/>
-                         <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"} suffix="@macalester.edu"
-                                    onGetErrorMessage={value => {
-                                        if (value==="") {
-                                            return 'This field is required';
-                                        }
-                                    }}/>
-                         <TextField label="Rent" required id={"rentBox"} onGetErrorMessage={value => {
-                             if (value==="") {
-                                 return 'This field is required';
-                             }
-                         }}/>
-                     </Stack>
+                     <div style={{display: "flex"}}>
+                         <Stack {...columnProps}>
+                             <TextField label="Name of Listing" autoAdjustHeight required id={"descriptionBox"} onGetErrorMessage={value => {
+                                 if (value === "") {
+                                     return 'This field is required';
+                                 }
+                             }}/>
+                             <TextField label="Name " required id={"nameBox"} onGetErrorMessage={value => {
+                                 if (value==="") {
+                                     return 'This field is required';
+                                 }
+                             }} />
+                             <TextField label="Listing Address" required id={"addressBox"} onGetErrorMessage={value => {
+                                 if (value==="") {
+                                     return 'This field is required';
+                                 }
+                             }}/>
+                         </Stack>
+                         <Stack {...columnProps2}>
+                             <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"} suffix="@macalester.edu"
+                                        onGetErrorMessage={value => {
+                                            if (value==="") {
+                                                return 'This field is required';
+                                            }
+                                        }}/>
+                             <TextField label="Rent" required id={"rentBox"} onGetErrorMessage={value => {
+                                 if (value==="") {
+                                     return 'This field is required';
+                                 }
+                             }}/>
+                             <TextField label="Short Paragraph for Details" multiline autoAdjustHeight required id={"detailBox"} onGetErrorMessage={value => {
+                                 if (value === "") {
+                                     return 'This field is required';
+                                 }
+                             }}/>
+                         </Stack>
+                     </div>
                      <br/>
                      <div style={{display: "flex"}}>
                      <div style={{marginLeft: '40%'}}>
@@ -250,7 +282,7 @@ class AddListings extends React.Component
                     <ul>
                         {this.state.listings[0].map((data) => (
                             <li key={data.address}>
-                                <SingleList description={data.description} name={data.name} address={data.address} email={data.email} rent={data.rent} image={data.photo}/>
+                                <SingleList description={data.description} name={data.name} address={data.address} email={data.email} rent={data.rent} image={data.photo} details={data.details}/>
                             </li>
                             ))}
                     </ul>
@@ -264,8 +296,9 @@ class AddListings extends React.Component
                             </section>
                         </div>
                     </div>
+                    <div style={{display: "flex"}}>
                     <Stack {...columnProps}>
-                        <TextField label="Short Description/Name of Listing" multiline autoAdjustHeight required id={"descriptionBox"} onGetErrorMessage={value => {
+                        <TextField label="Name of Listing" autoAdjustHeight required id={"descriptionBox"} onGetErrorMessage={value => {
                             if (value === "") {
                                 return 'This field is required';
                             }
@@ -280,6 +313,8 @@ class AddListings extends React.Component
                                 return 'This field is required';
                             }
                         }}/>
+                    </Stack>
+                    <Stack {...columnProps2}>
                         <TextField label="Contact Email" required mask="m\ask: @macalester.edu" id={"emailBox"} suffix="@macalester.edu"
                                    onGetErrorMessage={value => {
                                        if (value==="") {
@@ -291,7 +326,13 @@ class AddListings extends React.Component
                                 return 'This field is required';
                             }
                         }}/>
+                        <TextField label="Short Paragraph for Details" autoAdjustHeight required id={"detailBox"} onGetErrorMessage={value => {
+                            if (value === "") {
+                                return 'This field is required';
+                            }
+                        }}/>
                     </Stack>
+                    </div>
                     <br/>
                     <div style={{display: 'flex'}}>
                     <div style={{marginLeft: '40%'}}>
--- src/Components/Listings/SingleListingDisplay.js
+++ src/Components/Listings/SingleListingDisplay.js
@@ -1,28 +1,75 @@
 import React from 'react'
 import Box from '@material-ui/core/Box';
-import image from "../MainPage/Background1.jpeg";
-import { Stack} from '@fluentui/react/lib/Stack';
+import {IStackProps, Stack} from '@fluentui/react/lib/Stack';
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+
+
+const columnProps: Partial<IStackProps> = {
+    styles: { root: { maxWidth: "46vh" } },
+};
 
 class SingleList extends React.Component
 {
+    constructor(props) {
+        super(props);
+        this.state =
+            {
+                extend: true
+            }
+
+    }
+
+    setToTrue = () =>
+    {
+        this.setState(prevState => ({extend: !prevState.extend}));
+    }
+
     render()
     {
-        return (
-            <>
-                <Box bgcolor="white" p={1} border={2} marginRight={10} marginLeft={10}>
-                    <div style={{justifyContent: "space-between", display: "flex"}}>
-                        <Stack>
-                            <b style={{textAlign: "center", marginLeft: '70%', fontSize: '150%', marginRight: '-70%'}}>{this.props.description}</b>
-                            <p><b>Listing Address:</b> {this.props.address} </p>
-                            <p><b>Lister's Name:</b> {this.props.name}</p>
-                            <p><b>Lister's Contact:</b> {this.props.email}  </p>
-                            <p><b>Listing Rent:</b> {this.props.rent}  </p>
-                        </Stack>
-                        <img src={image} alt="Nothing" style={{width: '38vh', height: '25vh'}}/>
-                    </div>
-                </Box>
-            </>
-        );
+        // const img = document.getElementById("listing");
+        // img.setAttribute('src', this.props.image);
+        if (this.state.extend)
+        {
+            return (
+                <>
+                    <Box bgcolor="white" p={1} border={2} marginRight={10} marginLeft={10}>
+                        <b style={{textAlign: "center", fontSize: '150%', marginLeft: "25%"}}>{this.props.description}</b>
+                        <div style={{justifyContent: "space-between", display: "flex", fontSize: '2vh'}}>
+                            <Stack {...columnProps}>
+                                <p><b>Listing Address:</b> {this.props.address} </p>
+                                <p><b>Lister's Name:</b> {this.props.name}</p>
+                                <p><b>Lister's Contact:</b> {this.props.email}  </p>
+                                <p><b>Listing Rent:</b> {this.props.rent}  </p>
+                            </Stack>
+                            <img src={this.props.image} alt="Nothing" style={{width: '20%', height: '15%'}}/>
+                        </div>
+                        <PrimaryButton text="Extend" onClick={this.setToTrue} style={{width: '16vh', marginLeft: "45%"}} allowDisabledFocus/>
+                    </Box>
+                </>
+            );
+        }
+        else
+        {
+            return (
+                <>
+                    <Box bgcolor="white" p={1} border={2} marginRight={10} marginLeft={10}>
+                        <b style={{textAlign: "center", fontSize: '150%', marginLeft: "25%"}}>{this.props.description}</b>
+                        <div style={{justifyContent: "space-between", display: "flex", fontSize: '2vh'}}>
+                            <Stack {...columnProps}>
+                                <p><b>Listing Address:</b> {this.props.address} </p>
+                                <p><b>Lister's Name:</b> {this.props.name}</p>
+                                <p><b>Lister's Contact:</b> {this.props.email}  </p>
+                                <p><b>Listing Rent:</b> {this.props.rent}  </p>
+                                <b>Description: </b>
+                                <p style={{textAlign: "justify"}}>{this.props.details}</p>
+                            </Stack>
+                            <img src={this.props.image} alt="Nothing" style={{width: '20%', height: '15%'}}/>
+                        </div>
+                        <PrimaryButton text="Retract" onClick={this.setToTrue} style={{width: '16vh', marginLeft: "45%"}} allowDisabledFocus/>
+                    </Box>
+                </>
+            );
+        }
     }
 
 }
