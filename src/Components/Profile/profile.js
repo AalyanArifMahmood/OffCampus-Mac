e4cc4f2e6c65464a025a5e277eb839c95d9759b2
AalyanArifMahmood <85511135+AalyanArifMahmood@users.noreply.github.com>
2022-10-29 01:16:20 -0500

    Sorting is also operational now, plus sign on skeleton

--- src/Components/Profile/profile.js
+++ src/Components/Profile/profile.js
@@ -1,9 +1,57 @@
 import '../../CSS/index.css'
 import React from "react";
-// import {getAuth, GoogleAuthProvider, signInWithRedirect} from "firebase/auth";
-//
-// const auth = getAuth()
+import { PrimaryButton } from '@fluentui/react/lib/Button';
+import { initializeApp } from "firebase/app";
+import {GoogleAuthProvider,
+getAuth,
+ signInWithPopup,
+signOut}
+from "firebase/auth";
+
+import {getFirestore, query, getDocs, collection, where, addDoc,} from "firebase/firestore";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxyu7ou0WSzQYk63StiYtCVG-XtUPpqNs",
+    authDomain: "offcampusatmac.firebaseapp.com",
+    projectId: "offcampusatmac",
+    storageBucket: "offcampusatmac.appspot.com",
+    messagingSenderId: "546109308935",
+    appId: "1:546109308935:web:a52c2ee6d7cc1bae822aa2",
+    measurementId: "G-7CFVGFQXE8",
+    databaseURL: "https://offcampusatmac-default-rtdb.firebaseio.com/"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth()
+const db = getFirestore(app);
+
 // const provider = new GoogleAuthProvider()
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+    try {
+        const res = await signInWithPopup(auth, googleProvider);
+        const user = res.user;
+        const q = query(collection(db, "users"), where("uid", "==", user.uid));
+        const docs = await getDocs(q);
+        if (docs.docs.length === 0) {
+            await addDoc(collection(db, "users"), {
+                uid: user.uid,
+                name: user.displayName,
+                authProvider: "google",
+                email: user.email,
+            });
+        }
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+};
+
+const logout = () => {
+    signOut(auth);
+};
 
 export const Profile = React.FunctionComponent = () =>  {
     return (
@@ -13,7 +61,8 @@ export const Profile = React.FunctionComponent = () =>  {
                     <section id={"profile"}>
                         <h1 style={{fontSize: '6vh', fontFamily: 'Newslab, georgia, Bakersville', color: '#000000'}}>Your Profile</h1>
                     </section>
-
+                    <br/>
+                    <PrimaryButton text="Login" onClick={signInWithGoogle} allowDisabledFocus/>
                 </div>
             </div>
             <div className="separator" />
